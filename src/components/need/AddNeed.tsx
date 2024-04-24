09f4ec0ecb1e6ef2d095194f4a90a7d971bafa0c
Sam Murray <shm31094@gmail.com>
2024-04-24 11:30:27 -0400

    fille structure is better and we now have routes to files for each page

---
+++ src/components/need/AddNeed.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddNeed = () => {
+  return (
+    <div>AddNeed</div>
+  )
+}
---
+++ src/components/need/AllNeeds.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AllNeeds = () => {
+  return (
+    <div>AllNeeds</div>
+  )
+}
